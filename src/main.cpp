2a1ae40dee19b8b3b7e7dfb3c9fd45c28dfb10b9
Denis Golovanov <dmgolovanov@edu.hse.ru>
2021-03-23 17:24:25 +0300

    First attempt

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
-// Copyright 2020 A.SHT
+// Copyright 2021 Golovanov
 #include <iostream>
-#include <string>
-
 #include "postfix.h"
 
 int main() {
-  return 0;
+    std::string str_1 = "1 + 2";
+    std::cout << infix2postfix(str_1) << std::endl;
+    std::string str_2 = "2 + 6 * 3 / (4 - 2)";
+    std::cout << infix2postfix(str_2) << std::endl;
+    std::string str_3 = "12 - 6 * 4 - (56 - 9) / (3 + 5)";
+    std::cout << infix2postfix(str_3) << std::endl;
+    std::string str_4 = "45.67 - 7 / 8 * (4.3 + 8 / 4)";
+    std::cout << infix2postfix(str_4) << std::endl;
+    std::string str_5 = "((55 + 56) / (88 - 44) + (5 * 7 / 3))";
+    std::cout << infix2postfix(str_5) << std::endl;
+    return 0;
 }
--- src/postfix.cpp
+++ src/postfix.cpp
@@ -1,7 +1,81 @@
-// Copyright 2020 A.SHT
-#include "MyStack.h"
+﻿// Copyright 2021 Golovanov
 #include "postfix.h"
+#include "MyStack.h"
+
+int priority(char a) {
+    switch (a) {
+    case '(':
+        return 0;
+        break;
+    case ')':
+        return 1;
+        break;
+    case '+':
+        return 2;
+        break;
+    case '-':
+        return 2;
+        break;
+    case '*':
+        return 3;
+        break;
+    case '/':
+        return 3;
+        break;
+    default:
+        return -1;
+    }
+}
+
+bool check(char el) {
+    return (el >= '0' && el <= '9' || el == '.');
+}
 
 std::string infix2postfix(std::string infix) {
-  return infix;
+    MyStack<char> stack(500);
+    std::string result = "";
+    std::string tm = "";
+    for (int i = 0; i < infix.length(); i++) {
+        if (infix[i] == ' ') {
+            if (tm != "") {
+                result = result + tm + " ";
+                tm = "";
+            }
+            continue;
+        }
+        if (check(infix[i])) {
+            tm += infix[i];
+        }
+        if (priority(infix[i]) == 0) {
+            stack.push(infix[i]);
+        }
+        if (priority(infix[i]) == 1) {
+            if (tm != "") {
+                result = result + tm + " ";
+                tm = "";
+            }
+            while (stack.get() != '(') {
+                result = result + stack.pop() + " ";
+            }
+            stack.pop();
+        }
+        if (priority(infix[i]) == 2 || priority(infix[i]) == 3) {
+            if (stack.isEmpty() ||
+                priority(stack.get()) < priority(infix[i])) {
+                stack.push(infix[i]);
+            }
+            else {
+                result = result + stack.pop() + " ";
+                stack.push(infix[i]);
+            }
+        }
+    }
+    if (tm != "") {
+        result = result + tm + " ";
+    }
+    while (!stack.isEmpty()) {
+        result = result + stack.pop() + " ";
+    }
+    result.pop_back();
+    return result;
 }
